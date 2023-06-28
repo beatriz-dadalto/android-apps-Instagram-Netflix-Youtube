b0886947e142ac8053a376a63d95f2ed5864e7ac
Beatriz Dadalto <biacoelho@proton.me>
2023-06-28 18:12:29 -0300

    Intents e Activities

--- 4-FitnessTracker/FitnessTracker-start/app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ 4-FitnessTracker/FitnessTracker-start/app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,11 +1,23 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+            val i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+        }
     }
 }
