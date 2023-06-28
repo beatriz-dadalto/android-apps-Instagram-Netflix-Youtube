b8bcfc23e0167b1ef9eb13756a44dde8d38307c7
Beatriz Dadalto <biacoelho@proton.me>
2023-06-28 13:31:05 -0300

    Refatatação

--- 3-GanheiNaMega/GanheiNaMega/app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ 3-GanheiNaMega/GanheiNaMega/app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -2,14 +2,11 @@ package co.tiagoaguiar.ganheinamega
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Random
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,37 +35,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun numberGenerator(text: String, txtResult: TextView) {
-        if (text.isNotEmpty()) {
-            val quantity = text.toInt()
-            if (quantity in 6..15) {
-                val random = Random()
-                val numbers = mutableSetOf<Int>()
 
-                while (true) {
-                    val number = random.nextInt(60)
-                    numbers.add(number + 1)
-                    if (numbers.size == quantity) {
-                        break
-                    }
-                }
+        // VERIFICAR ERROS PRIMEIRO
 
-                txtResult.text = numbers.joinToString(" - ")
-            } else {
-                Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
-            }
-        } else {
+        // erro 1
+        if (text.isEmpty()) {
             Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
         }
 
+        // erro 2
+        val quantity = text.toInt()
+        if (quantity < 6 || quantity > 15) {
+            Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // AQUI EXECUTA SE NAO TIVER ERRO
+
+        val random = Random()
+        val numbers = mutableSetOf<Int>()
+
+        while (true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+            if (numbers.size == quantity) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
     }
 
-    // 1- via XML
+}
+
+// 1- via XML
 //    fun buttonClicked(view: View) {
 //        Log.i("TESTE", "Botão Clicado")
 //    }
 
-    // 2- variavel que seja do tipo View.OnClickListener (interface)
-    // quem chama o onClick é o proprio SDK do android que dispara apos o evento de touch
+// 2- variavel que seja do tipo View.OnClickListener (interface)
+// quem chama o onClick é o proprio SDK do android que dispara apos o evento de touch
 //    val buttonClickListner = View.OnClickListener { Log.i("TESTE", "Botão Clicado") }
 
-}
