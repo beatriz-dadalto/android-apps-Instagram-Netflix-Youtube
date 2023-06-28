cbd6019c4361b87434aaba13e091a255d5a346f3
Beatriz Dadalto <biacoelho@proton.me>
2023-06-28 20:54:23 -0300

    Validacao de formulários

--- 4-FitnessTracker/FitnessTracker-start/app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ 4-FitnessTracker/FitnessTracker-start/app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -2,10 +2,37 @@ package co.tiagoaguiar.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+    private lateinit var btnSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+        btnSend = findViewById(R.id.btn_imc_send)
+
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+        }
+    }
+
+    private fun validate(): Boolean {
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editHeight.text.toString().startsWith("0")
+                && !editWeight.text.toString().startsWith("0"))
     }
 }
+
