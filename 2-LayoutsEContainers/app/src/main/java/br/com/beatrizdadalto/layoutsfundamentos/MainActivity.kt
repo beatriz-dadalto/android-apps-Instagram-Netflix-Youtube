d066c27f36dd0a88b906d6fe8e7e3a0d56ea9000
Beatriz Dadalto <biacoelho@proton.me>
2023-07-28 17:24:30 -0300

    Conectando interface gráfica e lógica

--- 2-LayoutsEContainers/app/src/main/java/br/com/beatrizdadalto/layoutsfundamentos/MainActivity.kt
+++ 2-LayoutsEContainers/app/src/main/java/br/com/beatrizdadalto/layoutsfundamentos/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.beatrizdadalto.layoutsfundamentos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtUsername: TextView = findViewById(R.id.txt_username)
+        val txtEmail: TextView = findViewById(R.id.txt_email)
+
+        // aqui um monte de codigo que busca em um banco de dados
+        val username = "Beatriz Dadalto"
+
+        txtUsername.text = username
+        txtEmail.setText(R.string.send)
     }
 }
