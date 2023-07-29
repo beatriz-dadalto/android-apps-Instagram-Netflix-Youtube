1ccaece7b5b398a7fa1365a22f112f41833f6d96
Beatriz Dadalto <biacoelho@proton.me>
2023-07-28 22:27:18 -0300

    o ciclo de vida de uma Activity

--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -2,10 +2,36 @@ package co.tiagoaguiar.netflixremake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("TESTE", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("TESTE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("TESTE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("TESTE", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("TESTE", "onDestroy")
     }
 }
