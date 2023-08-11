8a4903c75be2063802b2c9fc60b765bb70dd1483
Beatriz Dadalto <biacoelho@proton.me>
2023-08-11 13:03:12 -0300

    Drawable com sombreamento

--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -1,8 +1,11 @@
 package co.tiagoaguiar.netflixremake
 
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,15 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // busquei o desenhavel (layer-list)
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        // busquei o filme que eu quero
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        // atribui a esse layer-list o novo filme
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+        // set no imageView
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
     }
 }
