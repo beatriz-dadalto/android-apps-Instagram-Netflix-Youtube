ab0d18da1c71cdc1c76df3ca0ab5ba7583452ee4
Beatriz Dadalto <biacoelho@proton.me>
2023-08-14 12:18:41 -0300

    Sincronizando ScrollView com RecyclerView

--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/CategoryAdapter.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/CategoryAdapter.kt
@@ -34,7 +34,7 @@ class CategoryAdapter(private val categories: List<Category>) : RecyclerView.Ada
 
             txtTitle.text = category.name
             rvCategory.layoutManager = LinearLayoutManager(itemView.context, RecyclerView.HORIZONTAL, false)
-            rvCategory.adapter = MovieAdapter(category.movies)
+            rvCategory.adapter = MovieAdapter(category.movies, R.layout.movie_item)
         }
     }
 }
--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -4,22 +4,46 @@ import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Movie
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
+        val txtTitle: TextView = findViewById(R.id.movie_txt_title)
+        val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
+        val txtCast: TextView = findViewById(R.id.movie_txt_cast)
+        val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        txtTitle.text = "Batman Begins"
+        txtDesc.text = "Essa é a descrição do filme"
+        txtCast.text = getString(R.string.cast, "Ator A, Ator B, Ator C")
+
+        val movies = mutableListOf<Movie>()
+        for (i in 0 until 15) {
+            val movie = Movie(R.drawable.movie)
+            movies.add(movie)
+        }
+
+        rv.layoutManager = GridLayoutManager(this, 3)
+        rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
+
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = null
 
         // busquei o desenhavel (layer-list)
-        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val layerDrawable: LayerDrawable =
+            ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
         // busquei o filme que eu quero
         val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
         // atribui a esse layer-list o novo filme
