67ebe7f4622d8506607d767e452721022866907a
Beatriz Dadalto <biacoelho@proton.me>
2023-08-22 18:12:11 -0300

    Parsers: convertendo estruturas de dados

--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
@@ -1,6 +1,9 @@
 package co.tiagoaguiar.netflixremake.util
 
 import android.util.Log
+import co.tiagoaguiar.netflixremake.model.Category
+import co.tiagoaguiar.netflixremake.model.Movie
+import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.ByteArrayOutputStream
 import java.io.IOException
@@ -17,10 +20,15 @@ class CategoryTask {
         val executor = Executors.newSingleThreadExecutor()
 
         executor.execute {
+
+            var urlConnection: HttpsURLConnection? = null
+            var buffer: BufferedInputStream? = null
+            var stream: InputStream? = null
+
             try {
                 // nesse momento estamos utilizando a NOVA THREAD [processo paralelo (2)]
                 val requestURL = URL(url) // abrir uma URL
-                val urlConnection =
+                urlConnection =
                     requestURL.openConnection() as HttpsURLConnection // abrir a conexão
                 urlConnection.readTimeout = 2000 // tempo de leitura (2 segundos)
                 urlConnection.connectTimeout = 2000 // tempo para conexão (2 segundos)
@@ -31,7 +39,7 @@ class CategoryTask {
                     throw IOException("Erro na comunicação com o servidor!")
                 }
 
-                val stream = urlConnection.inputStream // HTTP devolve uma sequência de bytes
+                stream = urlConnection.inputStream // HTTP devolve uma sequência de bytes
 
                 // forma 1: simples e rápida
 //                    val jsonAsString =
@@ -39,15 +47,45 @@ class CategoryTask {
 //                    Log.i("TESTE", jsonAsString)
 
                 // forma 2: bytes para string
-                val buffer = BufferedInputStream(stream)
+                buffer = BufferedInputStream(stream)
                 val jsonAsString = toString(buffer)
-                Log.i("TESTE", jsonAsString) // JSON pronto para ser convertido em um DATA CLASS!
+
+                // JSON pronto para ser convertido em um DATA CLASS!
+                val categories = toCategories(jsonAsString)
+                Log.i("TESTE", categories.toString())
 
             } catch (e: IOException) {
                 Log.e("TESTE", e.message ?: " Erro desconhecido", e)
+            } finally {
+                urlConnection?.disconnect()
+                stream?.close()
+                buffer?.close()
             }
-
         }
+    }
+
+    private fun toCategories(jsonAsString: String): List<Category> {
+        val categories = mutableListOf<Category>()
+
+        val jsonRoot = JSONObject(jsonAsString)
+        val jsonCategories = jsonRoot.getJSONArray("category")
+
+        // buscar categorias
+        for (i in 0 until jsonCategories.length()) {
+            val jsonCategory = jsonCategories.getJSONObject(i)
+            val title = jsonCategory.getString("title")
+            // buscar filmes
+            val jsonMovies = jsonCategory.getJSONArray("movie")
+            val movies = mutableListOf<Movie>()
+            for (j in 0 until jsonMovies.length()) {
+                val jsonMovie = jsonMovies.getJSONObject(j)
+                val id = jsonMovie.getInt("id")
+                val coverUrl = jsonMovie.getString("corver_url")
+                movies.add(Movie(id, coverUrl))
+            }
+            categories.add(Category(title, movies))
+        }
+        return categories
     }
 
     private fun toString(stream: InputStream): String {
