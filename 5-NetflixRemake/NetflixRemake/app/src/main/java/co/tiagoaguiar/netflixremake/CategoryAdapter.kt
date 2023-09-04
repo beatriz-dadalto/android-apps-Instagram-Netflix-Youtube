26395bcd1b07c6d2f0e358164aa3870132f424fc
Beatriz Dadalto <biacoelho@proton.me>
2023-09-04 03:52:32 -0300

    Intent e Click adapters

--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/CategoryAdapter.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/CategoryAdapter.kt
@@ -3,15 +3,13 @@ package co.tiagoaguiar.netflixremake
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Category
-import co.tiagoaguiar.netflixremake.model.Movie
 
 // aqui eh a lista VERTICAL
-class CategoryAdapter(private val categories: List<Category>) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+class CategoryAdapter(private val categories: List<Category>, private val onItemClickListener: (Int) -> Unit) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.category_item, parent, false)
@@ -34,7 +32,7 @@ class CategoryAdapter(private val categories: List<Category>) : RecyclerView.Ada
 
             txtTitle.text = category.name
             rvCategory.layoutManager = LinearLayoutManager(itemView.context, RecyclerView.HORIZONTAL, false)
-            rvCategory.adapter = MovieAdapter(category.movies, R.layout.movie_item)
+            rvCategory.adapter = MovieAdapter(category.movies, R.layout.movie_item, onItemClickListener)
         }
     }
 }
--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieAdapter.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieAdapter.kt
@@ -12,7 +12,11 @@ import co.tiagoaguiar.netflixremake.util.DownloadImageTask
 import com.squareup.picasso.Picasso
 
 // aqui eh a lista HORIZONTAL
-class MovieAdapter(private val movies: List<Movie>, @LayoutRes private val layoutId: Int) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+class MovieAdapter(
+    private val movies: List<Movie>,
+    @LayoutRes private val layoutId: Int,
+    private val onItemClickListener: ((Int) -> Unit)? = null
+) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
@@ -31,6 +35,10 @@ class MovieAdapter(private val movies: List<Movie>, @LayoutRes private val layou
     inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(movie: Movie) {
             val imgCover: ImageView = itemView.findViewById(R.id.img_cover)
+
+            imgCover.setOnClickListener {
+                onItemClickListener?.invoke(movie.id)
+            }
 
             DownloadImageTask(object : DownloadImageTask.Callback {
                 override fun onResult(bitmap: Bitmap) {
