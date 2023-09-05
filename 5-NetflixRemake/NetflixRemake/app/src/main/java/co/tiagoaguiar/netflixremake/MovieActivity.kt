d5d99b988d188eb86f226802af533070c8846902
Beatriz Dadalto <biacoelho@proton.me>
2023-09-05 16:36:57 -0300

    Detalhes de filmes

--- 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ 5-NetflixRemake/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -3,16 +3,22 @@ package co.tiagoaguiar.netflixremake
 import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Movie
+import co.tiagoaguiar.netflixremake.model.MovieDetail
+import co.tiagoaguiar.netflixremake.util.MovieTask
+import java.lang.IllegalStateException
 
-class MovieActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity(), MovieTask.Callback {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
@@ -22,9 +28,18 @@ class MovieActivity : AppCompatActivity() {
         val txtCast: TextView = findViewById(R.id.movie_txt_cast)
         val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
 
+        val id =
+            intent?.getIntExtra("id", 0) ?: throw IllegalStateException("ID não foi encontrado!")
+
+        val url =
+            "https://api.tiagoaguiar.co/netflixapp/movie/$id?apiKey=1d8f507d-9c2d-4678-a16e-ad416dd113b9"
+
+        // nesse momento vai chamar o url na outra Thread
+        MovieTask(this).execute(url)
+
         txtTitle.text = "Batman Begins"
-        txtDesc.text = "Essa é a descrição do filme"
-        txtCast.text = getString(R.string.cast, "Ator A, Ator B, Ator C")
+        txtDesc.text = "Essa é a descrição do filme do Batman"
+        txtCast.text = getString(R.string.cast, "Ator A, Ator B, Atriz A, Atriz B")
 
         val movies = mutableListOf<Movie>()
 
@@ -52,10 +67,22 @@ class MovieActivity : AppCompatActivity() {
 
     // disparar um evento sempre que um item da toolbar eh clicado
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // aqui tenho um item (imageView) do lado esquerdo que tem um id padrão
         if (item.itemId == android.R.id.home) {
             finish()
         }
         return super.onOptionsItemSelected(item)
     }
+
+    override fun onPreExecute() {
+
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResult(movieDetail: MovieDetail) {
+        Log.i("Teste", movieDetail.toString())
+    }
+
 }
