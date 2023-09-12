905c1d7c219d8771d1f52a62f7f45c05d290850a
Beatriz Dadalto <biacoelho@proton.me>
2023-09-12 12:53:21 -0300

    Sincronizando telas e menus com Controlador

--- 6-JokeApp-frameworks_modernos/app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/MainActivity.kt
+++ 6-JokeApp-frameworks_modernos/app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/MainActivity.kt
@@ -2,10 +2,48 @@ package co.tiagoaguiar.tutorial.jokerappdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        /*
+            Sincronizar telas e menus com Controlador
+         */
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+
+        // linkar o Host
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        // menu hamburguer
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_home, R.id.nav_joke_day, R.id.nav_about), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    // associar evento do clique no menu hamburguer
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
