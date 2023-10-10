041e72ae3df3f42474d2401cd77a42c4eb73b8b0
Beatriz Dadalto <biacoelho@proton.me>
2023-10-10 19:17:32 -0300

    Validação de Formulario

--- 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
+++ 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
@@ -3,10 +3,13 @@ package co.tiagoaguiar.course.instagram.login.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.tiagoaguiar.course.instagram.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
+
+    findViewById<TextInputLayout>(R.id.login_edit_email_input).error = "Esse e-mail é inválido"
   }
 }
