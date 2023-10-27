2a3b392f9dc0e2d86d37eef9c0c9882494426f7f
Beatriz Dadalto <biacoelho@proton.me>
2023-10-27 17:35:45 -0300

    Tela de upload de foto pela Câmera

---
+++ 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/camera/view/FragmentCamera.kt
@@ -0,0 +1,26 @@
+package co.tiagoaguiar.course.instagram.camera.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.course.instagram.R
+
+class FragmentCamera : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+
+}
