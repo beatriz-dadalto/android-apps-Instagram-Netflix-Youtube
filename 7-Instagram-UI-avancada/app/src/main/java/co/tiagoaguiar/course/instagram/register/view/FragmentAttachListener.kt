f58bed40b02bc7e7bfebc1db5491d3f83bd67a62
Beatriz Dadalto <biacoelho@proton.me>
2023-11-03 21:50:49 -0300

    Tela de envio de foto e ação de pular passo

--- 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/FragmentAttachListener.kt
+++ 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/FragmentAttachListener.kt
@@ -11,4 +11,5 @@ interface FragmentAttachListener {
     fun goToNameAndPasswordScreen(email: String)
     fun goToWelcomeScreen(name: String)
     fun goToPhotoScreen()
+    fun goToMainScreen()
 }
--- 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
+++ 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,5 +1,6 @@
 package co.tiagoaguiar.course.instagram.register.view
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -20,31 +21,48 @@ class RegisterPhotoFragment : Fragment(R.layout.fragment_register_photo) {
         -> lateinit var ja declara como nao nullable, ou seja, nao aceita null
      */
     private var binding: FragmentRegisterPhotoBinding? = null
-
-    override fun onDestroy() {
-        binding = null
-        super.onDestroy()
-    }
+    private var fragmentAttachListener: FragmentAttachListener? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentRegisterPhotoBinding.bind(view)
 
-        val customDialog = CustomDialog(requireContext())
-
-        customDialog.addButton(R.string.photo, R.string.gallery) {
-            when (it.id) {
-                R.string.photo -> {
-                    Log.i("TESTE", "foto uhull")
+        binding?.let {
+            with(it) {
+                registerBtnJump.setOnClickListener {
+                    fragmentAttachListener?.goToMainScreen()
                 }
 
-                R.string.gallery -> {
-                    Log.i("TESTE", "galeria ebaaa")
+                registerBtnNext.isEnabled = true
+                registerBtnNext.setOnClickListener {
+                    val customDialog = CustomDialog(requireContext())
+                    customDialog.addButton(R.string.photo, R.string.gallery) { dialogOption ->
+                        when (dialogOption.id) {
+                            R.string.photo -> {
+                                Log.i("TESTE", "foto uhull")
+                            }
+
+                            R.string.gallery -> {
+                                Log.i("TESTE", "galeria ebaaa")
+                            }
+                        }
+                    }
+                    customDialog.show()
                 }
             }
         }
+    }
 
-        customDialog.show()
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is FragmentAttachListener) {
+            fragmentAttachListener = context
+        }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
