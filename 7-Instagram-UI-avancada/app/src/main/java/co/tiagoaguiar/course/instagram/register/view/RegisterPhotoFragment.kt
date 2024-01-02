ec11053c95eb4d5232e2d5f77c67935cef0608b4
Beatriz Dadalto <biacoelho@proton.me>
2024-01-02 13:50:20 -0300

    hotfix: correcao de permissao para producao

--- 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
+++ 7-Instagram-UI-avancada/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,6 +1,8 @@
 package co.tiagoaguiar.course.instagram.register.view
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.graphics.Color
@@ -12,6 +14,8 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import co.tiagoaguiar.course.instagram.R
@@ -19,117 +23,148 @@ import co.tiagoaguiar.course.instagram.common.base.DependencyInjector
 import co.tiagoaguiar.course.instagram.common.view.CropperImageFragment
 import co.tiagoaguiar.course.instagram.common.view.CustomDialog
 import co.tiagoaguiar.course.instagram.databinding.FragmentRegisterPhotoBinding
+import co.tiagoaguiar.course.instagram.post.view.AddFragment
 import co.tiagoaguiar.course.instagram.register.RegisterPhoto
 import co.tiagoaguiar.course.instagram.register.presenter.RegisterPhotoPresenter
 
 class RegisterPhotoFragment : Fragment(R.layout.fragment_register_photo), RegisterPhoto.View {
 
-    /*
-          pq nao lateinit?
-              pq vai usar o ciclo de vida do fragment do onCreate e onDestroy para
-              livrar espaço de RAM para quando trocar de fragment.
-          -> lateinit var ja declara como nao nullable, ou seja, nao aceita null
-       */
-    private var binding: FragmentRegisterPhotoBinding? = null
-    private var fragmentAttachListener: FragmentAttachListener? = null
-    override lateinit var presenter: RegisterPhoto.Presenter
+   /*
+         pq nao lateinit?
+             pq vai usar o ciclo de vida do fragment do onCreate e onDestroy para
+             livrar espaço de RAM para quando trocar de fragment.
+         -> lateinit var ja declara como nao nullable, ou seja, nao aceita null
+      */
+   private var binding: FragmentRegisterPhotoBinding? = null
+   private var fragmentAttachListener: FragmentAttachListener? = null
+   override lateinit var presenter: RegisterPhoto.Presenter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
 
-        // recebe o caminho da imagem recortada
-        setFragmentResultListener("cropKey") { requestKey, bundle ->
-            val uri = bundle.getParcelable<Uri>(CropperImageFragment.KEY_URI)
-            onCropImageResult(uri)
-        }
-    }
+      // recebe o caminho da imagem recortada
+      setFragmentResultListener("cropKey") { requestKey, bundle ->
+         val uri = bundle.getParcelable<Uri>(CropperImageFragment.KEY_URI)
+         onCropImageResult(uri)
+      }
+   }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
 
-        binding = FragmentRegisterPhotoBinding.bind(view)
+      binding = FragmentRegisterPhotoBinding.bind(view)
 
-        val repository = DependencyInjector.registerEmailRepository()
-        presenter = RegisterPhotoPresenter(this, repository)
+      val repository = DependencyInjector.registerEmailRepository()
+      presenter = RegisterPhotoPresenter(this, repository)
 
-        binding?.let {
-            // com with -> forma enxuta. desse jeito não preciso colocar binding em todos elementos xml da tela
-            with(it) {
-                when (resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
-                    Configuration.UI_MODE_NIGHT_YES -> {
-                        // dark mode
-                        registerImageProfile.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN)
-                    }
-                    Configuration.UI_MODE_NIGHT_NO -> {
-                        // light mode
+      binding?.let {
+         // com with -> forma enxuta. desse jeito não preciso colocar binding em todos elementos xml da tela
+         with(it) {
+            when (resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+               Configuration.UI_MODE_NIGHT_YES -> {
+                  // dark mode
+                  registerImageProfile.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN)
+               }
 
-                    }
-                }
-                registerBtnJump.setOnClickListener {
-                    fragmentAttachListener?.goToMainScreen()
-                }
+               Configuration.UI_MODE_NIGHT_NO -> {
+                  // light mode
 
-                registerBtnNext.isEnabled = true
-                registerBtnNext.setOnClickListener {
-                    openDialog()
-                }
+               }
             }
-        }
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is FragmentAttachListener) {
-            fragmentAttachListener = context
-        }
-    }
-
-    override fun showProgress(enabled: Boolean) {
-        binding?.registerBtnNext?.showProgress(enabled)
-    }
-
-    override fun onUpdateFailure(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
-    }
-
-    override fun onUpdateSuccess() {
-        fragmentAttachListener?.goToMainScreen()
-    }
-
-    private fun openDialog() {
-        val customDialog = CustomDialog(requireContext())
-        customDialog.addButton(R.string.photo, R.string.gallery) {
-            when (it.id) {
-                R.string.photo -> {
-                    fragmentAttachListener?.goToCameraScreen()
-                }
-
-                R.string.gallery -> {
-                    fragmentAttachListener?.goToGalleryScreen()
-                }
+            registerBtnJump.setOnClickListener {
+               fragmentAttachListener?.goToMainScreen()
             }
-        }
-        customDialog.show()
-    }
 
-    private fun onCropImageResult(uri: Uri?) {
-        if (uri != null) {
-            val bitmap = if (Build.VERSION.SDK_INT >= 28) {
-                val source = ImageDecoder.createSource(requireContext().contentResolver, uri)
-                ImageDecoder.decodeBitmap(source)
-            } else {
-                MediaStore.Images.Media.getBitmap(requireContext().contentResolver, uri)
+            registerBtnNext.isEnabled = true
+            registerBtnNext.setOnClickListener {
+               openDialog()
             }
-            binding?.registerImageProfile?.setImageBitmap(bitmap)
-            binding?.registerImageProfile?.clearColorFilter()
-            presenter.updateUser(uri)
-        }
-    }
+         }
+      }
+   }
 
-    override fun onDestroy() {
-        binding = null
-        presenter.onDestroy()
-        super.onDestroy()
-    }
+   override fun onAttach(context: Context) {
+      super.onAttach(context)
+      if (context is FragmentAttachListener) {
+         fragmentAttachListener = context
+      }
+   }
+
+   override fun showProgress(enabled: Boolean) {
+      binding?.registerBtnNext?.showProgress(enabled)
+   }
+
+   override fun onUpdateFailure(message: String) {
+      Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+   }
+
+   override fun onUpdateSuccess() {
+      fragmentAttachListener?.goToMainScreen()
+   }
+
+   private fun openDialog() {
+      val customDialog = CustomDialog(requireContext())
+      customDialog.addButton(R.string.photo, R.string.gallery) {
+         when (it.id) {
+            R.string.photo -> {
+               if (allPermissionsGranted()) {
+                  fragmentAttachListener?.goToCameraScreen()
+               } else {
+                  getPermission.launch(REQUIRED_PERMISSION)
+               }
+            }
+
+            R.string.gallery -> {
+               fragmentAttachListener?.goToGalleryScreen()
+            }
+         }
+      }
+      customDialog.show()
+   }
+
+   private val getPermission =
+      registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { granted ->
+         if (allPermissionsGranted()) {
+            fragmentAttachListener?.goToCameraScreen()
+         } else {
+            Toast.makeText(
+               requireContext(),
+               R.string.permission_camera_denied,
+               Toast.LENGTH_LONG
+            ).show()
+         }
+      }
+
+   private fun allPermissionsGranted() =
+      ContextCompat.checkSelfPermission(
+         requireContext(),
+         REQUIRED_PERMISSION[0]
+      ) == PackageManager.PERMISSION_GRANTED
+
+   private fun onCropImageResult(uri: Uri?) {
+      if (uri != null) {
+         val bitmap = if (Build.VERSION.SDK_INT >= 28) {
+            val source = ImageDecoder.createSource(requireContext().contentResolver, uri)
+            ImageDecoder.decodeBitmap(source)
+         } else {
+            MediaStore.Images.Media.getBitmap(requireContext().contentResolver, uri)
+         }
+         binding?.registerImageProfile?.setImageBitmap(bitmap)
+         binding?.registerImageProfile?.clearColorFilter()
+         presenter.updateUser(uri)
+      }
+   }
+
+   override fun onDestroy() {
+      binding = null
+      presenter.onDestroy()
+      super.onDestroy()
+   }
+
+   companion object {
+      private val REQUIRED_PERMISSION = arrayOf(
+         Manifest.permission.CAMERA
+      )
+   }
 
 }
